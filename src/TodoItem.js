39737823bce212b0672bb2229e5efda6b6609a95
archtsv <thundersmile@126.com>
2019-05-03 19:18:53 +0800

    clean

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -18,37 +18,20 @@ class TodoItem extends Component {
 
   render() {
     const {
-      content,
-      test
+      content
     } = this.props
     return (
-      /*<li key={index} 
-          onClick={this.handleItemDelete.bind(this, index)}
-        >
-          {item}
-        </li>*/
-      // 双花括号，外层用于解析js表达式，内层花括号表示对象
-      // dangerouslySetInnerHTML防止输入的html标签被转义成字符串
-      // <li key={index} 
-      //   onClick={this.handleItemDelete.bind(this, index)}
-      //   dangerouslySetInnerHTML={{__html: item}}
-      // ></li>
       <li onClick={this.handleClick}>
-        {test} - { content }
+        { content }
       </li>
     )
   }
 }
 
 TodoItem.propTypes = {
-  test: PropTypes.string.isRequired,
   content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   deleteItem: PropTypes.func,
   index: PropTypes.number
-}
-
-TodoItem.defaultProps = {
-  test: 'hello react'
 }
 
 export default TodoItem
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,8 +3,6 @@ import React, {
   Fragment
 } from 'react'
 import TodoItem from './TodoItem'
-import Test from './Test'
-
 import './style.css'
 
 class TodoList extends Component {
@@ -21,21 +19,13 @@ class TodoList extends Component {
   }
 
   handleInputChange(e) {
-    // this.setState({
-    //   inputValue: e.target.value
-    // })
-    // const value = e.target.value
-    const value = this.input.value
+    const value = e.target.value
     this.setState(() => ({
       inputValue: value
     }))
   }
 
   handleBtnClick() {
-    // this.setState({
-    //   list: [...this.state.list, this.state.inputValue],
-    //   inputValue: ''
-    // })
     this.setState((state) => ({
       list: [...state.list, state.inputValue],
       inputValue: ''
@@ -43,13 +33,6 @@ class TodoList extends Component {
   }
 
   handleItemDelete(index) {
-    // immutable
-    // state 不允许我们做任何改变，因此将list拷贝出一个副本，操作副本
-    // 而不是直接操作 this.state 中的内容    
-    // const list = [...this.state.list]
-
-    // list.splice(index, 1)
-
     this.setState((state) => {
       const list = [...state.list]
       list.splice(index, 1)
@@ -74,25 +57,19 @@ class TodoList extends Component {
   }
 
   render() {
-    console.log('render')
     return (
-      <Fragment>
-        {/*jsx 注释*/}
-        <div>
+      <Fragment>        <div>
           <label htmlFor="insertArea">输入内容</label>
           <input type="text"
             id="insertArea"
             className="input" 
             value={this.state.inputValue}
-            onChange={this.handleInputChange}
-            ref={input => {this.input = input}}
-          />
+            onChange={this.handleInputChange}          />
           <button onClick={this.handleBtnClick}>提交</button>
         </div>
         <ul>
           { this.getTodoItem() }  
         </ul>
-        <Test content={this.state.inputValue} />
       </Fragment>
     )
   }

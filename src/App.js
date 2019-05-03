bf3e39d2f8a11519d4920d896834fd9cd811059a
archtsv <thundersmile@126.com>
2019-05-03 23:27:59 +0800

    project init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {
+  Component,
+  Fragment
+} from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: true
+    }
+    this.handleToggle = this.handleToggle.bind(this)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
+        <button onClick={this.handleToggle}>toggle</button>
+      </Fragment>
+    )
+  }
+
+  handleToggle() {
+    this.setState(() => {
+      return {
+        show: this.state.show ? false : true
+      }
+    })
+  }
+
+}
+
+export default App

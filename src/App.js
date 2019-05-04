3d67ea7ebc126ebfb99de15ce6d60c8c6a13376f
archtsv <thundersmile@126.com>
2019-05-04 12:21:05 +0800

    add transition-group

--- src/App.js
+++ src/App.js
@@ -3,30 +3,52 @@ import React, {
   Fragment
 } from 'react'
 
+import {
+  CSSTransition,
+  TransitionGroup
+} from 'react-transition-group'
+
 import './style.css'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      show: true
+      list: []
     }
-    this.handleToggle = this.handleToggle.bind(this)
+    this.handleAddItem = this.handleAddItem.bind(this)
   }
 
   render() {
     return (
       <Fragment>
-        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
-        <button onClick={this.handleToggle}>toggle</button>
+        <TransitionGroup>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition
+                  timeout={1000}
+                  classNames='fade'
+                  unmountOnExit
+                  onEntered={el => el.style.color='blue'}
+                  appear={true}
+                  key={index}
+                > 
+                  <div>{item}</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+        <button onClick={this.handleAddItem}>add item</button>
       </Fragment>
     )
   }
 
-  handleToggle() {
+  handleAddItem() {
     this.setState(() => {
       return {
-        show: this.state.show ? false : true
+        list: [...this.state.list, 'item']
       }
     })
   }

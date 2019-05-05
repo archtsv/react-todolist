6eba2450a1166274640aabe45521c3c74def7cce
cuijianlei <thundersmile@126.com>
2019-05-05 16:54:00 +0800

    add redux-thunk

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,18 +2,14 @@ import React, {
   Component
 } from 'react'
 
-import {
-  Input,
-  Button,
-  List
-} from 'antd'
-
 import store from './store'
 import {
+  getTodoList,
   getInputChangeAction,
   getAddItemAction,
   getDeleteItemAction
 } from './store/actionCreators'
+import TodoListUI from './TodoListUI'
 
 class TodoList extends Component {
 
@@ -23,29 +19,30 @@ class TodoList extends Component {
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleStoreChange = this.handleStoreChange.bind(this)
     this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.handleDeleteItem = this.handleDeleteItem.bind(this)
     store.subscribe(this.handleStoreChange)
   }
 
   render() {
-    return (
-      <div style={{marginTop: '10px', marginLeft: '10px'}}>
-        <div>
-          <Input
-            value={this.state.inputValue}
-            placeholder="todo info" 
-            style={{ width: '300px', marginRight: '10px' }}
-            onChange={this.handleInputChange}
+    return <TodoListUI 
+              inputValue={this.state.inputValue}
+              list={this.state.list}
+              handleInputChange={this.handleInputChange}
+              handleBtnClick={this.handleBtnClick}
+              handleDeleteItem={this.handleDeleteItem}
           />
-          <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
-        </div>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (<List.Item onClick={this.handleDeleteItem.bind(this, index)}>{item}</List.Item>)}
-        />
-      </div>
-    )
+  }
+
+  componentDidMount() {
+    // axios.get('/list.json')
+    //   .then((res) => {
+    //     const data = res.data
+    //     const action = initListAction(data)
+    //     store.dispatch(action)
+    //     console.log(res)
+    //   })
+    const action = getTodoList()
+    store.dispatch(action)
   }
 
   handleInputChange(e) {
--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,8 +1,10 @@
 import {
   CHANGE_INPUT_VALUE,
   ADD_TODO_ITEM,
-  DELETE_TODO_ITEM
+  DELETE_TODO_ITEM,
+  INIT_LIST
 } from './actionTypes'
+import axios from 'axios'
 
 export const getInputChangeAction = value => ({
   type: CHANGE_INPUT_VALUE,
@@ -17,3 +19,21 @@ export const getDeleteItemAction = index => ({
   type: DELETE_TODO_ITEM,
   index
 })
+
+export const initListAction = data => ({
+  type: INIT_LIST,
+  data
+})
+
+export const getTodoList = () => {
+  return (dispatch) => {
+    axios.get('/list.json')
+        .then((res) => {
+          const data = res.data
+          const action = initListAction(data)
+          dispatch(action)
+          // console.log(res)
+        })
+  }
+  
+}
